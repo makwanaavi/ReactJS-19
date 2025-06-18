fa04b215844fa12ecbe5e2fc8043253cf0244698
makwanaavi <makwanaavi73@gmail.com>
2025-06-18 13:47:29 +0530

    Dark And Light Mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
-import { UserProvider } from "./components/hooks/ContextApi";
-import { About } from "./components/hooks/ContextApi/About";
-import { Home } from "./components/hooks/ContextApi/Home";
+// import { UserProvider } from "./components/hooks/ContextApi";
+// import { About } from "./components/hooks/ContextApi/About";
+// import { Home } from "./components/hooks/ContextApi/Home";
+
+import {
+  ThemeProvider,
+  DarkLightMode,
+} from "./components/hooks/ContextApi/DarkLightMode";
 
 export const App = () => {
   return (
     <>
-      <UserProvider>
+      {/* <UserProvider>
         <Home />
         <br />
         <useBioContext/>
         <About />
-      </UserProvider>
-      
+      </UserProvider> */}
+      <ThemeProvider>
+        <DarkLightMode />
+      </ThemeProvider>
     </>
   );
 };
